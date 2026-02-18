26e02851522f6a9561c77ba2ba392cfce5c96697
MaritoSebastian <maritosebastianchavez1975@gmai.com>
2026-02-18 12:45:28 -0300

    se configuro de nuevo el cloudinary.config

--- api/index.js
+++ api/index.js
@@ -7,7 +7,9 @@ import { v2 as cloudinary } from "cloudinary";
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 cloudinary.config({
-   url:process.env.CLOUDINARY_URL,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
   secure: true,
 });
 const app = express();
